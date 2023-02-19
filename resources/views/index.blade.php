b36162657c70d5fd01eb9f47bc94a04007c92021
Marcela-Sevilla <marcelasevilla514@gmail.com>
2023-02-19 16:55:59 -0500

    Agregando template de Bootstrap y actualizando vista index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('contenido')
 
-<section class="card shadow-sm rounded-5">
+<!-- <section class="card shadow-sm rounded-5">
     <div class="card-header rounded-5 rounded-bottom-0">
         <h3 class="mb-0 ms-3"><i class="bi bi-clipboard-data-fill"></i> Registro de Compras</h3>
     </div>
@@ -45,7 +45,70 @@
         <a href="{{route('crear')}}" class="btn btn-outline-warning fs-5 shadow-sm border-2 mt-2 fw-semibold">Agregar Artículo <i class="bi bi-basket2-fill"></i></a>
 
     </div>
+</section> -->
+
+<div class="pagetitle">
+    <h1><i class="bi bi-clipboard-data-fill"></i> Registro de Compras</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item active">Registro de Compras</li>
+        </ol>
+    </nav>
+</div><!-- End Page Title -->
+
+<section class="section dashboard">
+    <div class="row justify-content-center">
+
+        <!-- Left side columns -->
+        <div class="col-lg-9">
+            <div class="row">
+
+                <!-- Recent Sales -->
+                <div class="col-12">
+                    <div class="card recent-sales overflow-auto">
+
+                        <div class="card-body">
+                            <h5 class="card-title">Ventas Recientes <span>| Hoy</span></h5>
+
+                            <table class="table table-borderless datatable">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Código</th>
+                                        <th scope="col">Artículo</th>
+                                        <th scope="col">Cantidad</th>
+                                        <th scope="col">Precio</th>
+                                        <th class="text-center" scope="col">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($articulos as $articulo)
+                                        <tr>
+                                            <td>{{$articulo->codigo}}</td>
+                                            <td>{{$articulo->descripcion}}</td>
+                                            <td>{{$articulo->contidad}}</td>
+                                            <td>${{$articulo->precio}}</td>
+                                            <td class="text-center">
+                                            <form action="/destroy/{{$articulo->id}}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                    <a href="/edit/{{$articulo->id}}" class="btn btn-sm btn-primary shadow-sm fw-semibold">Editar <i class="bi bi-pen-fill"></i></a>
+                                                    <button class="btn btn-sm btn-danger shadow-sm ms-2 fw-semibold">Eliminar <i class="bi bi-trash3-fill"></i></button>
+                                            </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                        </div>
+
+                    </div>
+                </div><!-- End Recent Sales -->
+
+            </div><!-- End Right side columns -->
+
+        </div>
 </section>
 
 @endsection
-
--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -1,39 +1,107 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Butterfly</title>
+
+    <title>Butterfly Clothing</title>
+
     <!-- FavIcon -->
     <link rel="shortcut icon" href="{{ asset('img/icon.png') }}" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
     <!-- Style Bootstrap -->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" />
     <!-- Bootstrap-Icons -->
-    <link rel="stylesheet" href="{{ asset('bootstrap-icons/bootstrap-icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" />
+    <!-- DataTable -->
+    <link rel="stylesheet" href="{{ asset('vendor/simple-datatables/style.css') }}" />
     <!-- Style -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-<body class="bg-light">
-    <header>
-        <nav class="navbar p-0">
-            <div class="container-fluid">
-                <a class="navbar-brand d-flex" href="/">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
-                <span class="fw-semibold fs-3 ms-2">Butterfly Clothing Store</span>
-                </a>
-            </div>
-        </nav>
-    </header>
 
-    <main class="container py-5">
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top pt-1">
+
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="index.html" class="logo d-flex align-items-center">
+                <img src="{{ asset('img/icon.png') }}" alt="Logo">
+                <span class="d-none d-lg-block">Butterfly Clothing</span>
+            </a>
+            <i class="bi bi-list toggle-sidebar-btn"></i>
+        </div><!-- End Logo -->
+
+        <nav class="header-nav ms-auto">
+            <ul class="d-flex align-items-center">
+
+                <li class="nav-item d-block d-lg-none">
+                    <a class="nav-link nav-icon search-bar-toggle " href="#">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </li><!-- End Search Icon-->
+
+            </ul>
+        </nav><!-- End Icons Navigation -->
+
+    </header><!-- End Header -->
+
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+
+        <ul class="sidebar-nav" id="sidebar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link " href="index.html">
+                    <i class="bi bi-clipboard-data-fill"></i>
+                    <span>Registro de Compras</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="{{route('crear')}}">
+                    <i class="bi bi-basket2-fill"></i>
+                    <span>Agregar Artículo</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="pages-contact.html">
+                    <i class="bi bi-envelope"></i>
+                    <span>Contact</span>
+                </a>
+            </li><!-- End Contact Page Nav -->
+
+        </ul>
+
+    </aside><!-- End Sidebar-->
+
+    <main id="main" class="main">
         @yield('contenido')
     </main>
 
-    <footer class="text-center text-secondary fw-semibold py-2 fixed-bottom">
-        <a href="https://github.com/Marcela-Sevilla" target="_blank" class="text-decoration-none text-secondary">Marcela Sevilla</a> ADSO-1 2023
-    </footer>
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </footer><!-- End Footer -->
 
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
+
 </html>
